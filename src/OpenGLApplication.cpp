cb3bab5b4df61aeb9ba9fbb0002ca4717df5c268
Brandon Burton <brandon@brandonbeta.com>
2016-08-29 16:47:30 -0700

    Changed default title to empty string

--- src/OpenGLApplication.cpp
+++ src/OpenGLApplication.cpp
@@ -7,9 +7,8 @@ OpenGLApplication::OpenGLApplication()
 
 void OpenGLApplication::Setup(EventHandler** handler, Renderer** renderer, Window** window)
 {
-	std::cout << "Got here" << std::endl;
 	glfwInit();
-	OpenGLWindow* newWindow = new OpenGLWindow(800, 600, "Engine Test");
+	OpenGLWindow* newWindow = new OpenGLWindow(800, 600, "");
 	Renderer* newRenderer = newWindow->GetRenderer();
 	OpenGLEventHandler* newHandler = new OpenGLEventHandler();
 
