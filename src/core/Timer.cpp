f029dec6217abb418470b154662007755c3e42e7
Brandon Burton <brandon@brandonbeta.com>
2017-02-17 16:15:07 -0800

    Added microseconds version of get tick to timer

--- src/core/Timer.cpp
+++ src/core/Timer.cpp
@@ -1,15 +1,25 @@
 #include "Timer.hpp"
 
-Timer::Timer() : m_StartTick(0)
+Timer::Timer()
 {
+	m_StartMTick = std::chrono::duration_cast<std::chrono::microseconds>(
+		std::chrono::system_clock::now().time_since_epoch()
+	).count();
+	m_StartMSTick = m_StartMTick / 1000;
+}
 
+uint64_t Timer::GetMicroTick()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(
+		std::chrono::system_clock::now().time_since_epoch()
+	).count() - m_StartMTick;
 }
 
 uint64_t Timer::GetTick()
 {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(
 		std::chrono::system_clock::now().time_since_epoch()
-	).count() - m_StartTick;
+	).count() - m_StartMSTick;
 }
 
 void Timer::Delay(uint64_t milliseconds)
--- src/core/Timer.hpp
+++ src/core/Timer.hpp
@@ -6,10 +6,12 @@
 class Timer
 {
 	protected:
-		uint64_t m_StartTick;
+		uint64_t m_StartMTick;
+		uint64_t m_StartMSTick;
 
 	public:
 		Timer();
+		uint64_t GetMicroTick();
 		uint64_t GetTick();
 		void Delay(uint64_t);
 };
